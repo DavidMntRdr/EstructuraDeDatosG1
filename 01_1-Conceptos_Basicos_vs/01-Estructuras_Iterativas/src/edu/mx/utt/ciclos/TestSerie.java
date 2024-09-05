00b889385ebe66a0aa206785419a31c44b9a0b69
David MR <david102867@outlook.com>
2024-09-05 12:58:30 -0600

    Logro: se realizó práctica en VisualStudio Code, creando la clase Serie con los métodos con for y while

---
+++ 01_1-Conceptos_Basicos_vs/01-Estructuras_Iterativas/src/edu/mx/utt/ciclos/TestSerie.java
@@ -0,0 +1,11 @@
+package edu.mx.utt.ciclos;
+
+public class TestSerie {
+    public static void main(String[] args) {
+        //double x = 0.0;
+        //int y =1;
+        //int z =2;
+        //x=(double)y/z;
+        //System.out.println(x);
+    }
+}
